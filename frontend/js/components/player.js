e1a97703c3317264d54e03d4bf5e4db0247e7d6a
Ilay <iazulay@cisco.com>
2018-04-01 19:03:32 +0300

    fixing a bug for using multiple players

--- frontend/js/components/player.js
+++ frontend/js/components/player.js
@@ -4,42 +4,60 @@ angular.module("app")
     controller: "playerCtrl"
 
   })
-  .controller('playerCtrl', ['$http', '$stateParams', function ($http, $stateParams) {
+  .service('playerCacheCounter', [function () {
+    var playerCounter = 0;
+
+    function increment() {
+      playerCounter++;
+    }
+
+    function getCounter() {
+      return playerCounter;
+    }
+
+    return {
+      increment: increment,
+      getCounter: getCounter
+    }
+  }])
+  .controller('playerCtrl', ['$http', '$state', '$stateParams', 'playerCacheCounter', function ($http, $state, $stateParams, playerCacheCounter) {
     var ctrl = this;
-    var indexPlayer = 0;
-    function createPlayer(rtspUrl){
-        indexPlayer++;
-        var playerId = 'vxg_media_player' + indexPlayer;
-        var div = document.createElement('div');
-        div.setAttribute("id", playerId);
-        div.setAttribute("class", "vxgplayer");
-        var runtimePlayers = document.getElementById('dynamicallyPlayers');
-        runtimePlayers.appendChild(div);
-        vxgplayer(playerId, {
-                url: rtspUrl,
-                nmf_path: 'media_player.nmf',
-                nmf_src: '../js/vxgplayer-1.8.31/pnacl/Release/media_player.nmf',
-                latency: 300000,
-                aspect_ratio_mode: 1,
-                autohide: 3,
-                controls: true,
-                connection_timeout: 5000,
-                connection_udp: 0,
-                custom_digital_zoom: false
-        }).ready(function(){
-            console.log(' =>ready player '+playerId);
-            vxgplayer(playerId).src(rtspUrl);
-            vxgplayer(playerId).play();
-            console.log(' <=ready player '+playerId);
-        });
+    var playerId = 'vxg_media_player';
+
+    function createPlayer(rtspUrl) {
+      var calculatedId = playerId + playerCacheCounter.getCounter();
+      playerCacheCounter.increment();
+      var div = document.createElement('div');
+      div.setAttribute("id", calculatedId);
+      div.setAttribute("class", "vxgplayer");
+      var runtimePlayers = document.getElementById('dynamicallyPlayers');
+      runtimePlayers.appendChild(div);
+      vxgplayer(calculatedId, {
+        url: rtspUrl,
+        nmf_path: 'media_player.nmf',
+        nmf_src: '../js/vxgplayer-1.8.31/pnacl/Release/media_player.nmf',
+        latency: 300000,
+        aspect_ratio_mode: 1,
+        autohide: 3,
+        controls: true,
+        connection_timeout: 5000,
+        connection_udp: 0,
+        custom_digital_zoom: false
+      }).ready(function () {
+        console.log(' =>ready player ' + calculatedId);
+        player = vxgplayer;
+        vxgplayer(calculatedId).src(rtspUrl);
+        vxgplayer(calculatedId).play();
+        console.log(' <=ready player ' + calculatedId);
+      });
     }
 
     ctrl.$onInit = function () {
-        if ($stateParams.rtspUrl) {
-            createPlayer('rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov')
-        }
-        else {
-            alert('URL is missing')
-        }
+      if ($stateParams.rtspUrl) {
+        createPlayer($stateParams.rtspUrl)
+      } else {
+        alert('URL is missing');
+        $state.go('list');
+      }
     }
   }])
